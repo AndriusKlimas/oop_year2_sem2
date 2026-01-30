3b5ca377b6fe9e616a6d6a66c63a1935cb069b8e
Andrius Klimas <andrius.klimas01@gmail.com>
2026-01-30 11:30:03 +0000

    added validation for password, email and username, in customer.py

--- relearning_classes/relearning_basic_functions/customers.py
+++ relearning_classes/relearning_basic_functions/customers.py
@@ -1,7 +1,15 @@
 class Customers:
     def __init__(self, username, password, email):
+        if not Customers.validate_username(username):
+            print("Username provided is incorrect")
         self._username = username
+
+        if not Customers.validate_password(password):
+            print("Password provided is incorrect")
         self.__password = password
+
+        if not Customers.validate_email(email):
+            print("Email provided is incorrect")
         self._email = email
 
 
@@ -24,3 +32,48 @@ class Customers:
 
     def get_username(self):
         return self._username
+
+
+    #username validation method
+    @staticmethod
+    def validate_username(username):
+        if username is None:
+            return False
+        if len(username.strip()) < 8:
+            return False
+        return True
+
+
+
+    #password validation method
+    @staticmethod
+    def validate_password(password):
+        #If the password is None, return False
+        if password is None:
+            return False
+        #If the password length is less than 8, return False
+        if len(password.strip()) < 8:
+            return False
+
+        # Check for at least one uppercase and one lowercase letter
+        #If any character in the password is uppercase, set upper to true, do same for lower
+        upper_check = any(char.isupper() for char in password)
+        if not upper_check:
+            print("Password must contain at least one uppercase letter.")
+            return False
+
+        lower_check = any(char.islower() for char in password)
+        if not lower_check:
+            print("Password must contain at least one lowercase letter.")
+            return False
+
+        return True
+
+    #email validation method
+    @staticmethod
+    def validate_email(email):
+        if email is None:
+            return False
+        if "@" not in email or "." not in email:
+            return False
+        return True
