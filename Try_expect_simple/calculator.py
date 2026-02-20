742f26d3f8abef852df5734a94481f203a0a6687
Andrius Klimas <andrius.klimas01@gmail.com>
2026-02-20 12:21:24 +0000

    added simple error handling example in calculator file

---
+++ Try_expect_simple/calculator.py
@@ -0,0 +1,60 @@
+def multiply():
+    valid = False
+    while not valid:
+        try:
+            num1 = float(input("Enter first number: "))
+            valid = True
+        except ValueError as e:
+            print("Please enter a valid number.")
+#so that the num 1 is saved and only num 2 is chucking errors
+    valid = False
+    while not valid:
+        try:
+            num2 = float(input("Enter second number: "))
+            valid = True
+        except ValueError as e:
+            print("Please enter a valid number.")
+
+
+    print(num1 * num2)
+
+def add():
+    valid = False
+    while not valid:
+        try:
+            num1 = float(input("Enter first number: "))
+            valid = True
+        except ValueError as e:
+            print("Please enter a valid number.")
+
+    valid = False
+    while not valid:
+        try:
+            num2 = float(input("Enter second number: "))
+            valid = True
+        except ValueError as e:
+            print("Please enter a valid number.")
+
+
+    print(num1 + num2)
+
+keepRunning = True
+
+while keepRunning:
+    print("Choose from the following: ")
+    print("1) Add two numbers")
+    print("2) Multiply two numbers")
+    print("exit to exit the calculate")
+    choice = input()
+    match(choice.lower()):
+        case "1":
+            add()
+        case "2":
+            multiply()
+        case "exit":
+            print("Goodbye!")
+            keepRunning = False
+            continue
+        case _:
+            print("Please enter a valid option from the menu")
+
--- relearning_classes/relearning_basic_functions_and_adding_on/customers.py
+++ relearning_classes/relearning_basic_functions_and_adding_on/customers.py
@@ -1,15 +1,17 @@
+from types import NotImplementedType
+
 class Customers:
     def __init__(self, username, password, email):
         if not Customers.validate_username(username):
-            print("Username provided is incorrect")
+            raise ValueError("Username provided is incorrect")
         self._username = username
 
         if not Customers.validate_password(password):
-            print("Password provided is incorrect")
+            raise ValueError("Password provided is incorrect")
         self.__password = password
 
         if not Customers.validate_email(email):
-            print("Email provided is incorrect")
+            raise ValueError("Email provided is incorrect")
         self._email = email
 
 
@@ -18,6 +20,7 @@ class Customers:
 
     def display(self):
         print(f"get_username(), get_email()")
+
 
     def __repr__(self):
         print(f"get_username(), get_email(), get_password()")
@@ -116,3 +119,83 @@ class Customers:
         if "@" not in email or "." not in email:
             return False
         return True
+
+if __name__ == '__main__':
+
+    # print("Creating cust 1 (bad password)")
+    # cust1 = Customers("michelle", "password", "michelle@password")
+    # print(f"cust1: {cust1}")
+    # print(f"repr for cust1: {repr(cust1)}")
+    # print(f"hash for cust1: {hash(cust1)}")
+    # print("")
+    #
+    # print("Creating cust2 (bad email)")
+    # cust2 = Customers("hermione", "Wing4rdium", "hermione_email")
+    # print(f"cust2: {cust2}")
+    # print(f"repr for cust2: {repr(cust2)}")
+    # print(f"hash for cust2: {hash(cust2)}")
+    # print("")
+    #
+    # print("Creating cust3 (bad username)")
+    # cust3 = Customers("shorty", "SuperS3cur3", "short@accepted.com")
+    # print(f"cust3: {cust3}")
+    # print(f"repr for cust3: {repr(cust3)}")
+    # print(f"hash for cust3: {hash(cust3)}")
+    # print("")
+    #
+    # print("Creating cust4 (All good!)")
+    # cust4 = Customers("valid_username", "Valid passw0rd", "valid_email@emaildomain.com")
+    # print(f"cust4: {cust4}")
+    # print(f"repr for cust4: {repr(cust4)}")
+    # print(f"hash for cust4: {hash(cust4)}")
+    # print("")
+    #
+    # print("Creating cust5 (Values are valid, same username as cust1)")
+    # cust5 = Customers("michelle", "Valid passw0rd", "valid_email@emaildomain.com")
+    # print(f"cust5: {cust5}")
+    # print(f"repr for cust5: {repr(cust5)}")
+    # print(f"hash for cust5: {hash(cust5)}")
+    # print("")
+    #
+    # if cust1 != cust2:
+    #     print(f"cust1 and cust2 have different usernames (cust1: {cust1.get_username()}, cust2: {cust2.get_username()}), "
+    #           f"therefore can't be the same user")
+    # else:
+    #     print("cust1 and cust2 have the same username - this shouldn't have happened!")
+    #
+    # if cust1 == cust5:
+    #     print(f"cust1 and cust5 have the same username (cust1: {cust1.get_username()}, cust5: {cust5.get_username()}), "
+    #           f"therefore are considered the same entity")
+    # else:
+    #     print("cust1 and cust5 have different usernames - this shouldn't have happened!")
+    #
+    # # Check where customer is not less than
+    # if cust1 < cust2:
+    #     print(f"{cust1.get_username()} is less than {cust2.get_username()}")
+    # else:
+    #     print(f"{cust1.get_username()} is not less than {cust2.get_username()}")
+    #
+    # # Check where customer is less than
+    # if cust1 < cust4:
+    #     print(f"{cust1.get_username()} is less than {cust4.get_username()}")
+    # else:
+    #     print(f"{cust1.get_username()} is not less than {cust2.get_username()}")
+    #
+    # # Check where customer is not less than because they are equal
+    # if cust1 < cust5:
+    #     print(f"{cust1.get_username()} is less than {cust5.get_username()}")
+    # else:
+    #     print(f"{cust1.get_username()} is not less than {cust5.get_username()}")
+
+
+    try:
+        cust10 = Customers("michelle", "Password1", "michelle@password")
+
+    except ValueError as e:
+        print(e)
+
+    try:
+        customers = Customers("michelle", "password", "")
+
+    except ValueError as e:
+        print(e)
