bd9a0694787a4edd0e26a09a4be530336e22e756
Andrius Klimas <andrius.klimas01@gmail.com>
2026-02-26 12:11:51 +0000

    added classes, added vlaidtion

--- Homework1/ExceptionExerciseCode_1/code_1/products.py
+++ Homework1/ExceptionExerciseCode_1/code_1/products.py
@@ -1,4 +1,47 @@
 class Product:
+    def __init__(self, id, name, cost_price, retail_price, quantity):
+
+        if Product.id_validation(id):
+            self.id = id
+        else:
+            raise ValueError("Invalid product id")
+
+        if Product.name_validation(name):
+            self.name = name
+        else:
+            raise ValueError("Invalid product name")
+
+
+        self.name = name
+        self.cost_price = cost_price
+        self.retail_price = retail_price
+        self.quantity = quantity
+
+    def __str__(self):
+        print(f"id = {self.id}, name = {self.name}, cost_price = {self.cost_price}, retail_price = {self.retail_price}, quantity = {self.quantity}")
+
+    def __repr__(self):
+        print(f"id = {self.id}, name = {self.name}, cost_price = {self.cost_price}, retail_price = {self.retail_price}, quantity = {self.quantity}")
+
+
+    @staticmethod
+    def id_validation(id):
+        if id < 0:
+            return False
+
+        if id is None:
+            return False
+
+        else:
+            return True
+
+    @staticmethod
+    def name_validation(name):
+        if name is None:
+            return False
+        else:
+            return True
+
     '''
         Code for the Product class goes here
         Product has:
@@ -21,3 +64,9 @@ class Book(Product):
         an author
         one or more genres (these should be stored in a list)
     '''
+if __name__ == "__main__":
+    print("Testing ID as -1, should come with error")
+    try:
+        product1 = Product(-1,"wheel",1,1,1)
+    except ValueError as e:
+        print(e)
