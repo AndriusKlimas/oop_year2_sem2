a3ac368da7facd8d45a3af7b7e1412851ca4dd44
Andrius Klimas <andrius.klimas01@gmail.com>
2026-03-11 18:18:44 +0000

    set all getters

--- Homework1/Week6/week6_homework_class.py
+++ Homework1/Week6/week6_homework_class.py
@@ -3,12 +3,24 @@ from types import NotImplementedType
 
 class Student():
     def __init__(self, student_id:str, name:str, subject:str, grade:int):
-        self.student_id = student_id
-        self.name = name
-        self.subject = subject
-        self.grade = grade
+        self._student_id = student_id
+        self._name = name
+        self._subject = subject
+        self._grade = grade
 
 
-    #Getting the things returned
+    #Returning the iteams so that they can be used
+
+    def get_student_id(self):
+        return self._student_id
+
+    def get_name(self):
+        return self._name
+
+    def get_subject(self):
+        return self._subject
+
+    def get_grade(self):
+        return self._grade
 
 
