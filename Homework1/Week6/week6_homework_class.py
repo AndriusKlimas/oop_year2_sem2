3ce2dd75f81a6838438c431dd0b211e92b0a93b9
Andrius Klimas <andrius.klimas01@gmail.com>
2026-03-11 18:16:57 +0000

    setting up the class

---
+++ Homework1/Week6/week6_homework_class.py
@@ -0,0 +1,14 @@
+from types import NotImplementedType
+
+
+class Student():
+    def __init__(self, student_id:str, name:str, subject:str, grade:int):
+        self.student_id = student_id
+        self.name = name
+        self.subject = subject
+        self.grade = grade
+
+
+    #Getting the things returned
+
+
