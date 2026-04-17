d274f2a9f01ebda2c7213900336b93e7f9b8a933
Andrius Klimas <andrius.klimas01@gmail.com>
2026-04-17 11:41:09 +0100

    added new layered design

---
+++ g_layered_design/ticket_service.py
@@ -0,0 +1,14 @@
+from tickets import Ticket
+class TicketService:
+    def __init__(self, assigned: dict[str, list[Ticket]] = None):
+        self.__assigned_tickets = dict.copy(assigned) if assigned else {}
+
+    def get_tickets_for_agent(self, agent: str):
+        if agent.lower() not in self.__assigned_tickets:
+            return None
+
+        return  self.__assigned_tickets[agent.lower()]
+
+
+    def get_agents(self):
+        return list(self.__assigned_tickets.keys())
