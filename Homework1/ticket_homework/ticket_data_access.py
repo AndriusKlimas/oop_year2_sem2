f43ba63551ef1f3c0412e4be6e75a35a8268b348
Andrius Klimas <andrius.klimas01@gmail.com>
2026-04-30 09:27:39 +0100

    code: added parse_tickets, all info was put into a text file before deleting it from main file

--- Homework1/ticket_homework/ticket_data_access.py
+++ Homework1/ticket_homework/ticket_data_access.py
@@ -26,3 +26,26 @@ class TicketDataAccess:
                     logger.info(f"Assigned ticket retrieved for {agent}: {ticket}")
 
         return unassigned_tickets, assigned_tickets
+
+    def parse_ticket(self, text: str) -> Ticket | None:
+        parts = text.strip().split("%%")
+
+        try:
+            ticket_type = parts[0]
+            ticket_id = int(parts[1])
+            title = parts[2]
+            desc = parts[3]
+            status = parts[4]
+            assigned_to = parts[5]
+
+            if ticket_type.lower() == "ticket":
+                return self.build_ticket(ticket_id, title, desc, status, assigned_to)
+            else:
+                requested = parts[6]
+                approval = parts[7]
+                return self.build_feature_request(ticket_id, title, desc, status, assigned_to, requested, approval,
+                                                  text)
+
+        except Exception as e:
+            logger.warning(f"line is skipped: {text}")
+            return None
