b44ce5f9a7d2542f4ce05d8d6ce104c772d9ef4a
Andrius Klimas <andrius.klimas01@gmail.com>
2026-04-30 09:23:10 +0100

    updated the files and created initial file

--- Homework1/ticket_homework/ticketing_system.py
+++ Homework1/ticket_homework/ticketing_system.py
@@ -5,6 +5,7 @@ import logging
 import logging.config
 import json
 from ticket_service import TicketService
+from ticket_data_access import TicketDataAccess   # <-- NEW IMPORT
 
 def configure_logging_json() -> None:
     with open("logging_config.json") as f:
@@ -16,113 +17,33 @@ configure_logging_json()
 logger = logging.getLogger(__name__)
 
 
-def parse_ticket(text: str) -> Ticket | None:
-    line = text.strip().split("%%")
-    # Line formats:
-    # TICKET%%id%%title%%desc%%status%%assigned_to
-    # FEATURE%%id%%title%%desc%%status%%assigned_to%requested%%approval%%status
-
+def load_ticket_model() -> TicketService | None:
+    filename = ""
     try:
-        ticket_type = line[0]
+        filename = input("Please enter ticket data filename: ")
 
-        try:
-            ticket_id = int(line[1])
-        except ValueError as e:
-            logger.error(f"Ticket id ({line[1]}) is in incorrect format - must be an int")
-            print(f"Record is malformed [invalid ticket id] - skipping: {text}")
-            return None
+        data_access = TicketDataAccess()
+        unassigned, assigned = data_access.read_file(filename)
 
-        title= line[2]
-        desc = line[3]
-        status = line[4]
-        assigned_to = line[5]
+        ticket_service = TicketService(assigned, unassigned)
+        return ticket_service
 
-        if ticket_type.upper() == "TICKET":
-            ticket = build_ticket(assigned_to, desc, status, ticket_id, title)
-            return ticket
-        else:
-            requested_feat = line[6]
-            approval_status = line[7]
-
-            feature_ticket = build_feature_request(approval_status, assigned_to, desc, text, requested_feat, status,
-                                           ticket_id, title)
-            return feature_ticket
-
-    except IndexError as e:
-        logger.warning(f"Malformed line - insufficient components provided. Line: {text}")
-        print(f"Record is malformed [missing components] - skipping: {text}")
-        return None
-    except TicketException as e:
-        logger.error(f"Issue with ticket data: {e} - skipping record: {text}")
-        print(f"Issue with ticket data - skipping record: {text}")
-        return None
-    except ValueError as e:
-        logger.warning(f"Missing information in line {text} - {e}")
-        print(f"Record is malformed [missing components] - skipping: {text}")
+    except FileNotFoundError:
+        logger.warning(f"Cannot open file {filename}")
+        print(f"File {filename} cannot be found. Please enter a new filename.")
         return None
 
 
-def build_feature_request(approval_status: str, assigned_to: str, desc: str, line: str, requested_feat: str,
-                          status: str, ticket_id: int, title: str) -> FeatureRequest:
-    ticket = FeatureRequest(ticket_id, title, desc, requested_feat)
-    ticket.update_status(status)
-    ticket.assign_to(assigned_to)
-    match approval_status.upper():
-        case "APPROVED":
-            ticket.approve()
-        case "REJECTED":
-            ticket.reject()
-        case "PENDING":
-            pass
-        case _:
-            raise TicketException(f"Illegal approval status ({approval_status}) supplied - skipping record: {line}")
-    return ticket
+def display_ticket_list(ticket_service: TicketService) -> None:
+    ticket_list = ticket_service.get_unassigned_tickets()
+    if len(ticket_list) == 0:
+        print("No unassigned tickets found")
+        return
 
-
-def build_ticket(assigned_to: str, desc: str, status: str, ticket_id: int, title: str):
-    ticket = Ticket(ticket_id, title, desc)
-    ticket.update_status(status)
-    if assigned_to != "":
-        ticket.assign_to(assigned_to)
-    return ticket
-
-
-def read_file(filename: str) -> tuple[list[Ticket], dict[str, list[Ticket]]]:
-    unassigned_tickets = []
-    assigned_tickets = {}
-
-    # Read file
-    with open(filename) as file:
-        for line in file:
-            ticket = parse_ticket(line)
-            if ticket is None:
-                continue
-            # If ticket is not assigned, place in list of unassigned tickets
-            assigned_agent = ticket.get_assigned_agent()
-            if assigned_agent == "":
-                unassigned_tickets.append(ticket)
-                # Compliance logging - ticket stored
-                logger.info(f"Unassigned ticket added to queue: {ticket}")
-            else:
-                assign_ticket(assigned_agent, assigned_tickets, ticket)
-
-    return unassigned_tickets, assigned_tickets
-
-
-def assign_ticket(assigned_agent: str, assigned_tickets: dict[str, list[Ticket]], ticket: Ticket):
-    # Otherwise, place in dictionary, associated with assigned agent
-    agent_list = assigned_tickets.get(assigned_agent.lower(), [])
-    agent_list.append(ticket)
-    assigned_tickets[assigned_agent.lower()] = agent_list
-    # Compliance logging - ticket stored and assigned
-    logger.info(f"Assigned ticket to {assigned_agent} - ticket details: {ticket}")
-
-
-def display_ticket_list(ticket_list: list[Ticket]) -> None:
-    returned_info = TicketService.get_ticket_list()
-    if returned_info is None:
-        print("No ticket list")
-    print(returned_info)
+    print("Unassigned tickets: ")
+    for i, ticket in enumerate(ticket_list, 1):
+        print(f"{i}) {ticket}")
+        logger.info(f"Ticket details viewed: {ticket}")
 
 
 def display_agents(ticket_service: TicketService) -> None:
@@ -145,15 +66,19 @@ def display_tickets_for_agent(ticket_service: TicketService) -> None:
             logger.info(f"Ticket details viewed: {ticket}")
 
 
-def assign_next_ticket(unassigned_list: list[Ticket], assigned_dict: dict[str, list[Ticket]]) -> None:
-    agent = input("Please enter agent name: ")
-
-    info = ticket_service.assign_next_ticket_1(agent)
-
-    if info is None:
-        print("No tickets can be assigned.")
-
-    print(info)
+def assign_next_ticket(ticket_service: TicketService) -> None:
+    agent = input("Enter agent name to assign next ticket: ")
+    try:
+        assigned = ticket_service.assign_next_ticket(agent)
+        if not assigned:
+            logger.info(f"No tickets currently available - could not assign to {agent}")
+            print("No unassigned tickets available")
+    except TicketException as e:
+        logger.error(f"Attempting to assign previously assigned ticket: {e}")
+        print(f"Cannot assign ticket - ticket is already assigned")
+    except ValueError as e:
+        logger.error(f"Attempting to assign ticket to illegal agent value: {e}")
+        print(f"Cannot assign ticket - agent \"{agent}\" is invalid. Please try again with a different agent")
 
 
 def display_menu() -> None:
@@ -166,26 +91,7 @@ def display_menu() -> None:
     print("exit) Exit the program")
 
 
-if __name__ == "__main__":
-    # Read file:
-    valid = False
-    filename = ""
-    while not valid:
-        try:
-            filename = input("Please enter ticket data filename: ")
-            unassigned, assigned = read_file(filename)
-            valid = True
-        except FileNotFoundError as e:
-            logger.warning(f"Cannot open file {filename}")
-            print(f"File {filename} cannot be found. Please enter a new filename.")
-
-    print("Unassigned data:")
-    for t in unassigned:
-        print(t)
-
-    ticket_service = TicketService(assigned, unassigned)
-
-    # Run main application logic
+def run_ui(ticket_service: TicketService):
     keep_running = True
     while keep_running:
         print("=" * 20)
@@ -197,11 +103,21 @@ if __name__ == "__main__":
             case "2":
                 display_tickets_for_agent(ticket_service)
             case "3":
-                print("Unassigned tickets: ")
-                display_ticket_list(unassigned)
+                display_ticket_list(ticket_service)
             case "4":
-                assign_next_ticket(unassigned, assigned)
+                assign_next_ticket(ticket_service)
             case "exit":
                 keep_running = False
             case _:
                 print("Invalid option selected")
+
+
+if __name__ == "__main__":
+    valid = False
+    ticket_service = None
+    while not valid:
+        ticket_service = load_ticket_model()
+        if ticket_service:
+            valid = True
+
+    run_ui(ticket_service)
